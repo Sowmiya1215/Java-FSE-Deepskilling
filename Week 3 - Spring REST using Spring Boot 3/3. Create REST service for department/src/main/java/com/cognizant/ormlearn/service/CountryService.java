0358ec630e4a00af372f7c8b53765f256f934990
Sowmiya <165918178+Sowmiya1215@users.noreply.github.com>
2026-07-08 22:07:24 +0530

    Additional HandsOn

---
+++ Week 3 - Spring REST using Spring Boot 3/3. Create REST service for department/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,53 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode)
+            throws CountryNotFoundException {
+
+        Optional<Country> result = countryRepository.findById(countryCode);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country Not Found");
+        }
+
+        return result.get();
+    }
+
+    @Transactional
+    public List<Country> searchCountries(String text) {
+        return countryRepository.findByNameContaining(text);
+    }
+
+    @Transactional
+    public List<Country> searchCountriesSorted(String text) {
+        return countryRepository.findByNameContainingOrderByNameAsc(text);
+    }
+
+    @Transactional
+    public List<Country> getCountriesStartingWith(String letter) {
+        return countryRepository.findByNameStartingWith(letter);
+    }
+}
