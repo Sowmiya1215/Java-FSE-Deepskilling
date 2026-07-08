74d65a85b10ce019927dd477a4a1b65234f10287
Sowmiya <165918178+Sowmiya1215@users.noreply.github.com>
2026-07-08 22:17:06 +0530

    Added Week 3 - Spring REST using Spring Boot 3

---
+++ Week 3 - Spring REST using Spring Boot 3/3. Create REST service for department/src/main/java/com/cognizant/ormlearn/service/DepartmentService.java
@@ -0,0 +1,24 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.ormlearn.dao.DepartmentDao;
+import com.cognizant.ormlearn.model.Department;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    @Transactional
+    public ArrayList<Department> getAllDepartments() {
+
+        return departmentDao.getAllDepartments();
+
+    }
+}
