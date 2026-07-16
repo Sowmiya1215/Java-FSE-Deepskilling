110de060b9681553a206d790404578598db079b2
Sowmiya <165918178+Sowmiya1215@users.noreply.github.com>
2026-07-16 09:19:29 +0530

    Added Week 4 Microservices

---
+++ Week 4 - Microservices/Microservices for account and loan/Create Eureka Discovery Server and register/account/account/src/main/java/controller/AccountController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/account")
+    public String getAccount() {
+        return "Account Service is Running";
+    }
+
+}
