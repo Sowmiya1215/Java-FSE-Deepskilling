deafd1b39f3939abd06bbf07bb9803d14dd8ed6f
Sowmiya <165918178+Sowmiya1215@users.noreply.github.com>
2026-07-21 20:36:14 +0530

    Added Week 5 React

---
+++ Week 5 - React/6. ReactJS-HOL/trainersapp/src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to Cognizant Academy Trainers Portal</h2>
+            <p>Select Trainers from the navigation menu.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ Week 5 - React/6. ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,10 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+    new Trainer(1,"John","john@gmail.com","9876543210","Java","Spring, Hibernate"),
+    new Trainer(2,"David","david@gmail.com","9876543211","Python","Django, Flask"),
+    new Trainer(3,"Mary","mary@gmail.com","9876543212","React","React, Redux"),
+    new Trainer(4,"James","james@gmail.com","9876543213","Angular","Angular, TypeScript")
+];
+
+export default trainers;
